a339827169a502301f358cdc48ae120b37272c19
Tebogo Mashika <tebogo@gmail.com>
2021-09-03 20:04:34 +0200

    new project

---
+++ src/main/java/com/codeAvengers/quizProject/Model/QuizMaster.java
@@ -0,0 +1,15 @@
+package com.codeAvengers.quizProject.Model;
+
+
+// QuizMaster - is our entity
+// the POJO will be persisted into the database which is the h2-console
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "QuizMaster")
+public class QuizMaster {
+
+
+}
---
+++ src/test/java/com/codeAvengers/quizProject/QuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeAvengers.quizProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

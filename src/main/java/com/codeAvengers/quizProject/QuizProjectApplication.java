4a204ed75ffe9bba9b3d78cad3722bd3eb4908ca
Louis Nel <83593106+CreepyTubeSocks@users.noreply.github.com>
2021-09-07 16:18:33 +0200

    Add files via upload
    - Added code for Game Pin (as comments until implemented along spring data).

--- src/main/java/com/codeAvengers/quizProject/QuizProjectApplication.java
+++ src/main/java/com/codeAvengers/quizProject/QuizProjectApplication.java
@@ -10,4 +10,16 @@ public class QuizProjectApplication {
 		SpringApplication.run(QuizProjectApplication.class, args);
 	}
 
+
+	// THIS PART GENERATES THE 5 RANDOM NUMBERS FOR THE GAME PIN..
+
+	//int gamePin = getRandomInt(100000, 10000);
+	//System.out.println("Your game pin is: " + gamePin);
+
+	/* * returns random integer between minimum and maximum range */
+	//public static int getRandomInt(int maximum, int minimum) {
+		//return ((int) (Math.random() * (maximum - minimum))) + minimum;
+	//}
+
+
 }
